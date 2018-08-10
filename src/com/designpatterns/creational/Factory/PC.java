dbb3413d5c0994fb9baf0c377f2208d6e07955f9
Harshit <harshitacharya.kadekaru@itcinfotech.com>
2018-08-10 16:12:24 +0530

    Factory design pattern

---
+++ src/com/designpatterns/creational/Factory/PC.java
@@ -0,0 +1,30 @@
+package com.designpatterns.creational.Factory;
+
+public class PC extends Computer{
+	
+	private String ram;
+	private String hdd;
+	private String cpu;
+	
+	public PC(String ram, String hdd, String cpu) {
+		super();
+		this.ram = ram;
+		this.hdd = hdd;
+		this.cpu = cpu;
+	}
+
+	@Override
+	public String getRAM() {
+		return this.ram;
+	}
+
+	@Override
+	public String getHDD() {
+		return this.hdd;
+	}
+
+	@Override
+	public String getCPU() {
+		return this.cpu;
+	}
+}
